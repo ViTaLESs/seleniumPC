43fbb45e075920227669c56e1062bc38a2f67366
ViTaLES <vitalbka.91@gmail.com>
2016-06-15 21:14:59 +0300

    added hometask11

---
+++ src/main/java/pages/CheckoutPage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by ViTaLES on 27.05.2016.
+ */
+public class CheckoutPage {
+
+
+    public CheckoutPage(WebDriver driver) {
+
+    }
+
+    public void confirmOrder() {
+
+    }
+}
---
+++ src/test/java/tests/firstTestPO.java
@@ -0,0 +1,86 @@
+package tests;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import pages.ProductPage;
+import utils.WebElementsActions;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ViTaLES on 30.05.2016.
+ */
+public class firstTestPO {
+
+    private static WebDriver driver;
+    private String baseUrl = "http://www.ellos.se/";
+    static WebElementsActions web;
+
+
+    @FindBy(id = "j_idt5:nome")
+    private WebElement inputNome;
+    //private WebElement Input_Name = driver.findElement(By.id("vcxcsfdfsf"));
+
+    @FindBy(id = "j_idt5:idade")
+    private WebElement inputIdade1;
+
+    @FindBy(id = "j_idt5:nome")
+    private WebElement inputNome2;
+
+    @FindBy(id = "j_idt5:idade")
+    private WebElement inputIdade3;
+
+    @FindBy(id = "j_idt5:nome")
+    private WebElement inputNome4;
+
+    @FindBy(id = "j_idt5:idade")
+    private WebElement inputIdade5;
+
+    @FindBy(id = "j_idt5:nome")
+    private WebElement inputNome6;
+
+    @FindBy(id = "j_idt5:idade")
+    private WebElement inputIdade7;
+
+
+
+    @Test
+    public void digitarTexto() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException {
+
+        inputNome.sendKeys("Diego");
+        inputIdade5.sendKeys("29");
+        inputIdade5.sendKeys("29");
+        inputIdade5.sendKeys("29");
+        inputIdade5.sendKeys("29");
+        inputIdade5.sendKeys("29");
+        //...
+
+
+        ProductPage productPage = new ProductPage(driver);
+        productPage.clickOnSizeDD();
+        productPage.checkDDLabelsPresent();
+
+    }
+
+
+
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        web = new WebElementsActions(driver);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}
